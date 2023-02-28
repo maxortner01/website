35a37a8e44c7844656652dde191ae7d97f5dd5a8
Max Ortner <maxortner01@gmail.com>
2023-02-28 13:13:11 -0600

    fixing fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Link from 'next/link';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <Link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <Link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
