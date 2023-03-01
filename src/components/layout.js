efc880639dd237b4d1c9a67a756d63fcc8650d74
Max Ortner <maxortner01@gmail.com>
2023-02-28 22:14:38 -0600

    fixed link on main page

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
       <div className='flex flex-col h-screen'>
       <Link href="/"><div className="transistion duration-75 text-right px-5 py-20 hover:bg-gradient-to-r from-transparent to-slate-200  hover:text-slate-700 hover:border-r-8 border-slate-400 hover:px-3 hover:font-extrabold">Max Ortner</div></Link>
         <div className='grow flex flex-col-reverse pb-5'>
-        <Link href="https://Linkrxiv.org/search/?query=Max+Ortner&searchtype=all&source=header"><div className="transistion duration-75 p-3 text-right px-5 hover:bg-gradient-to-r from-transparent to-slate-200  hover:text-slate-700 hover:border-r-8 border-slate-400 hover:px-3">arxiv</div></Link>
+        <Link href="https://arxiv.org/search/?query=Max+Ortner&searchtype=all&source=header"><div className="transistion duration-75 p-3 text-right px-5 hover:bg-gradient-to-r from-transparent to-slate-200  hover:text-slate-700 hover:border-r-8 border-slate-400 hover:px-3">arxiv</div></Link>
         <Link href="https://github.com/maxortner01"><div className="transistion duration-75 p-3 text-right px-5 hover:bg-gradient-to-r from-transparent to-slate-200  hover:text-slate-700 hover:border-r-8 border-slate-400 hover:px-3">GitHub</div></Link>
         <Link href="/resume.pdf"><div className="transistion duration-75 p-3 text-right px-5 hover:bg-gradient-to-r from-transparent to-slate-200  hover:text-slate-700 hover:border-r-8 border-slate-400 hover:px-3">Resume</div></Link>
         <Link href="/posts"><div className="transistion duration-75 p-3 text-right px-5 hover:bg-gradient-to-r from-transparent to-slate-200  hover:text-slate-700 hover:border-r-8 border-slate-400 hover:px-3">Blog</div></Link>
